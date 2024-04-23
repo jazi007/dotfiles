21dcb9328439c395fd0d36dc5add9e9b6c9e031c
Nadhmi JAZI <nadhmi.jazi@gmail.com>
2024-04-23 12:45:58 +0200

    fix(nvim): inlay fix

--- nvim/lua/plugins/rust.lua
+++ nvim/lua/plugins/rust.lua
@@ -1,26 +1,26 @@
 return {
-	"mrcjkb/rustaceanvim",
-	version = "^4", -- Recommended
-	ft = { "rust" },
-	config = function()
-		vim.g.rustaceanvim = {
-			inlay_hints = {
-				highlight = "NonText",
-			},
-			tools = {
-				hover_actions = {
-					auto_focus = true,
-				},
-			},
-			server = {
-				on_attach = function(client, bufnr)
-					if client.server_capabilities.inlayHintProvider then
-						-- TODO: check why this is not correctly visible with nord
-						vim.cmd("highlight LspInlayHint guifg=#B3B6B7")
-						vim.lsp.inlay_hint.enable(bufnr, true)
-					end
-				end,
-			},
-		}
-	end,
+  "mrcjkb/rustaceanvim",
+  version = "^4", -- Recommended
+  ft = { "rust" },
+  config = function()
+    vim.g.rustaceanvim = {
+      inlay_hints = {
+        highlight = "NonText",
+      },
+      tools = {
+        hover_actions = {
+          auto_focus = true,
+        },
+      },
+      server = {
+        on_attach = function(client, bufnr)
+          if client.server_capabilities.inlayHintProvider then
+            -- TODO: check why this is not correctly visible with nord
+            vim.cmd("highlight LspInlayHint guifg=#B3B6B7")
+            vim.lsp.inlay_hint.enable(true)
+          end
+        end,
+      },
+    }
+  end,
 }
